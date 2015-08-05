ce761a97cc8f765bbc9695618e551f9911a50f87
MadsHolten <mads.h.rasmussen@gmail.com>
2015-08-05 17:49:01 +0200

    Google maps tilføjet

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
 //Det er en god idé at ramme JS-koden ind
 (function(){
+	'use strict';
 	//Modul defineres
-	var app = angular.module('geoCode', ["firebase"]);
+	angular
+		//Moduler er beholdere for forskellige dele af appen – controllere, services, filtre, directives, etc.
+		.module('geoCode', ["firebase"])
 
-	//CONTROLLER TIL AT HENTE GEOCODE DATA VIA GOOGLES API
 
-	//Scopes bruges til at dele data mellem controllers og views (dependency injection)
-	app.controller('GetGeoCode', ['$scope','$http', function($scope, $http){
-	//app.controller('GetGeoCode', ['$scope','$http', 'fbCRUD', function($scope, $http, fbCRUD){	
+		//CONTROLLER TIL AT HENTE GEOCODE DATA VIA GOOGLES API
 
-		$scope.coordinates = 'Indtast addresse eller sted';
+		//Scopes bruges til at dele data mellem controllers og views (dependency injection)
+		.controller('GetGeoCode', ['$scope','$http', function($scope, $http){
+		//app.controller('GetGeoCode', ['$scope','$http', 'fbCRUD', function($scope, $http, fbCRUD){	
 
-		$scope.update = function(geo) {
+			$scope.coordinates = 'Indtast addresse eller sted';
 
-			//Kode til Google develop
-			var googleAPIkey = 'AIzaSyDPmpy8aXArbc3gCXMjA3y-0g7OBJWzgOk';
+			$scope.update = function(geo) {
 
-			//Addressefelt hentes
-			var adr = $scope.geo.address.split(' ').join('+');
+				//Kode til Google develop
+				var googleAPIkey = 'AIzaSyDPmpy8aXArbc3gCXMjA3y-0g7OBJWzgOk';
 
-			//$scope-variablen er bare et JS objekt. Der kan sættes variable uden at skulle kalde en funktion eller noget
-		    $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + adr + '&key=' + googleAPIkey)
-		    	.success(function(data) {
-		    		if(data.status == 'OK') {
-						// this callback will be called asynchronously
-						// when the response is available
-						var adr = data.results[0].formatted_address;
-						var lat = data.results[0].geometry.location.lat;
-						var lng = data.results[0].geometry.location.lng;
+				//Addressefelt hentes
+				var adr = $scope.geo.address.split(' ').join('+');
 
-						$scope.coordinates = adr + ' | ' + lat + ', ' + lng;
+				//$scope-variablen er bare et JS objekt. Der kan sættes variable uden at skulle kalde en funktion eller noget
+			    $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + adr + '&key=' + googleAPIkey)
+			    	.success(function(data) {
+			    		if(data.status == 'OK') {
+							// this callback will be called asynchronously
+							// when the response is available
+							var adr = data.results[0].formatted_address;
+							var lat = data.results[0].geometry.location.lat;
+							var lng = data.results[0].geometry.location.lng;
 
-						//fbCRUD.addPOI(data);
-					}
-					else {$scope.coordinates = 'Ingen resultater'}
-				});
-		};
-	}]);
-/*
-	app.factory('fbCRUD', ['$firebase', function($firebase) {
-		//Opret forbindelse til firebase
-		var ref = new Firebase("https://rdtrp.firebaseio.com");
+							$scope.coordinates = adr + ' | ' + lat + ', ' + lng;
 
-		var addPOI = function(POI) {
-			POIs.$add(data);
-		};
-	}]);*/
+							//fbCRUD.addPOI(data);
+						}
+						else {$scope.coordinates = 'Ingen resultater'}
+					});
+			};
+		}]);
+	/*
+		app.factory('fbCRUD', ['$firebase', function($firebase) {
+			//Opret forbindelse til firebase
+			var ref = new Firebase("https://rdtrp.firebaseio.com");
+
+			var addPOI = function(POI) {
+				POIs.$add(data);
+			};
+		}]);*/
 
 })();
